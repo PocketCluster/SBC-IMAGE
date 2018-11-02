93c2de3a8ff206f41db30d1fe468ceb39e6b639e
Sung-Taek, Kim <stkim1@colorfulglue.com>
2018-11-02 20:53:09 +0900

    sdcard friendly fstype

--- BUILDER/PINE64-V3/make-image.sh
+++ BUILDER/PINE64-V3/make-image.sh
@@ -76,10 +76,8 @@ EOM
 
     # creating filesystems
     mkfs.ext2 ${XTARGET}p1
-    mkfs.ext4 ${XTARGET}p2
-
     # https://blogofterje.wordpress.com/2012/01/14/optimizing-fs-on-sd-card/
-    #mkfs.ext4 -F -O ^has_journal -E stride=2,stripe-width=1024 -b 4096 -L PC_ROOT -U ${FS_ROOT_UUID} -m 5 "${ROOT_LOOP}"
+    mkfs.ext4 -F -O ^has_journal -E stride=2,stripe-width=1024 -b 4096 -L PC_ROOT -U ${FS_ROOT_UUID} -m 5 ${XTARGET}p2
 
     # setup rootfs
     mkdir -p ${MOUNTDIR}
